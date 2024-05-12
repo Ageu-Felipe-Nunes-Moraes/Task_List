68354b0b0e30c85bd8cc9a7bb21e1b7d576694b5
Ageu Moraes <ageumoraes67@gmail.com>
2024-05-12 20:08:34 -0300

    Comentando o código javascript em inglês

--- Task_List/script.js
+++ Task_List/script.js
@@ -1,27 +1,28 @@
-// Constante para armazenar a chave usada para acessar o armazenamento local
+
+// Constant for store the key using to access the local storage
 const local_storage_key = 'to-do-list-gn'
 
-// Classe para manipular a lista de tarefas
+// Class to manipulate the task list
 class TaskList {
     constructor() {
-        // Inicialização do objeto com os valores armazenados localmente ou um array vazio
+        // Object initialization with locally stored values or an empty array
         this.values = JSON.parse(localStorage.getItem(local_storage_key) || "[]")
     }
 
-    // Método para validar se uma tarefa já existe na lista
+    // Method to validate whether a task exists in the list
     validate_if_task_exist(){     
         let input_value = document.getElementById("input-new-task").value
-        // Verifica se já existe uma tarefa com o mesmo nome
+        // Checks if there is a task with one same name
         let exist      = this.values.find(x => x.name == input_value)
         return !exist ? false : true
     }
 
-    // Método para adicionar uma nova tarefa à lista
+    // Method to add a new task to the list
     new_task(){
         let input = document.getElementById("input-new-task")
         input.style.border = ""
 
-        // Validação do campo de entrada
+        // Input field validation
         if(!input.value){
             input.style.border = "1px solid red"
             alert("Digite algo para inserir em sua lista")
@@ -30,23 +31,23 @@ class TaskList {
             alert("Já existe uma tarefa com essa descrição")
         }
         else{
-            // Adiciona a nova tarefa à lista
+            // Add new task to list
             this.values.push({
                 name: input.value
             })
-            // Atualiza o armazenamento local com a lista atualizada
+            // Updates local storage with list update
             localStorage.setItem(local_storage_key,JSON.stringify(this.values))
             this.shows_values()
         }
-        // Limpa o campo de entrada após adicionar a tarefa
+        // Clears the input field after adding the task
         input.value = ""
     }
 
-    // Método para exibir os valores da lista na interface
+    // Method to show values of the list in the interface
     shows_values(){
         let list = document.getElementById('to-do-list')
         list.innerHTML = ''
-        // Itera sobre os valores da lista e os exibe na interface
+        // Iterates over the values in the list and displays them in the interface
         for(let i = 0; i < this.values.length; i++){
             list.innerHTML += `<li>${this.values[i]['name']} <button id='btn-ok' onclick='task.remove_item("${this.values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
             <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
@@ -54,20 +55,19 @@ class TaskList {
         }
     }
 
-    // Método para remover um item da lista
+    // Method to remove item from list
     remove_item(item){
-        // Encontra o índice do item na lista
+        // Finds the index of the item in the list
         let index = this.values.findIndex(x => x.name == item)
-        // Remove o item da lista
+        // Remove the item from the list
         this.values.splice(index, 1)
-        // Atualiza o armazenamento local com a lista atualizada
+        // Updates local storage with the updated list
         localStorage.setItem(local_storage_key,JSON.stringify(this.values))
         this.shows_values()
     }
 }
 
-// Instância da classe TaskList
+// TaskList class instance
 let task = new TaskList();
-// Exibe os valores da lista na inicialização
+// Show the values of the list in the initialization
 task.shows_values();
-
